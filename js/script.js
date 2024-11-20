028896166c4e1fd7695a09ec0d0524e9feafc994
caffeineeee <cevinsam11@gmail.com>
2024-11-20 17:44:48 +0700

    Update footer

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
 function whatsapp() {
-  var name = document.getElementById("name").value;
-  var phone = document.getElementById("phone").value;
-  var message = document.getElementById("message").value;
-  var submit = document.getElementById("submit");
+	const name = document.getElementById("name").value;
+	const phone = document.getElementById("phone").value;
+	const message = document.getElementById("message").value;
+	const submit = document.getElementById("submit");
 
-  if (!name || !phone || !message) {
-    submit.disabled = true;
-  } else {
-    var url =
-      "https://wa.me/6281364496579?text=" +
-      "*Kritik atau Saran pada Website Resmi Kampung SENGKEMANG*%0a%0a" +
-      "*Nama Pengirim:* " +
-      name +
-      "%0a" +
-      "*No. Telepon/WhatsApp Pengirim:* " +
-      phone +
-      "%0a" +
-      "*Kritik atau Saran Pengirim:* " +
-      message;
+	if (!name || !phone || !message) {
+		submit.disabled = true;
+	} else {
+		const url = `https://wa.me/6281364496579?text=*Kritik atau Saran pada Website Resmi Kampung SENGKEMANG*%0a%0a*Nama Pengirim:* ${name}%0a*No. Telepon/WhatsApp Pengirim:* ${phone}%0a*Kritik atau Saran Pengirim:* ${message}`;
 
-    window.open(url, "_blank").focus();
-  }
-  return submit.disabled = false;
+		window.open(url, "_blank").focus();
+	}
+	submit.disabled = false;
+	return;
 }
+
+function updateDomToCurrentYear() {
+	const currentYear = new Date().getFullYear();
+	document.getElementById("dynamic-year").innerHTML = currentYear;
+}
+
+window.onload = () => {
+	updateDomToCurrentYear();
+};
